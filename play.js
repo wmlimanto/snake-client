0de1ed49bf8e7bbff2678a201424b4835334d334
Winston Limanto <wmlimanto@gmail.com>
2022-04-16 16:51:09 -0700

    added connect function that sets up a connection and returns an object

--- play.js
+++ play.js
@@ -1,2 +1,24 @@
 //main file through which we will launch the game client
 
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.1.47',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //added event for when ded
+  conn.on("data", () => {
+    console.log("you ded cuz you idled bruh")
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
