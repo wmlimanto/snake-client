0ffa2d61459576f33d4037b3331658d9a57bdc0a
Winston Limanto <wmlimanto@gmail.com>
2022-04-16 18:11:03 -0700

    added setupInput function to handle user input

--- play.js
+++ play.js
@@ -1,9 +1,29 @@
 //main file through which we will launch the game client
 
 const net = require("net");
+const { stdin } = require("process");
 
 // establishes a connection with the game server
 const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+
+  //ctrl+c to exit the game
+  if (key === '\u0003') {
+    process.exit();
+  };
+}
