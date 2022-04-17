3a14262a77b7d423a1d69b210da527c778d46e59
Winston Limanto <wmlimanto@gmail.com>
2022-04-16 18:29:57 -0700

    moved setupInput function into input.js and exported function

---
+++ input.js
@@ -0,0 +1,20 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+
+  //ctrl+c to exit the game
+  if (key === '\u0003') {
+    process.exit();
+  };
+}
+
+module.exports = { setupInput };
