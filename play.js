6590c0ef698ed87a4ce941aa65dde66da056722a
Winston Limanto <wmlimanto@gmail.com>
2022-04-16 18:31:36 -0700

    refactored code-- imports setupInput function from input.js

--- play.js
+++ play.js
@@ -4,26 +4,11 @@ const net = require("net");
 const { stdin } = require("process");
 
 // establishes a connection with the game server
-const connect = require('./client')
+const { connect } = require("./client");
+// setup interface to handle user input from stdin
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-
-  //ctrl+c to exit the game
-  if (key === '\u0003') {
-    process.exit();
-  };
-}
+setupInput();
