5bfa8892bae710d81c1628d98b47d637fc2073a2
Winston Limanto <wmlimanto@gmail.com>
2022-04-16 19:33:47 -0700

    added char o, p, l to say messages

--- input.js
+++ input.js
@@ -18,21 +18,28 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   };
-
+  //WASD movement
   if (key === 'w') {
     connection.write("Move: up");
   };
-
   if (key === 'a') {
     connection.write("Move: left");
   };
-
   if (key === 's') {
     connection.write("Move: down");
   };
-
   if (key === 'd') {
     connection.write("Move: right");
+  };
+  //say message
+  if (key === 'o') {
+    connection.write("Say: OOPS!");
+  };
+  if (key === 'p') {
+    connection.write("Say: :P");
+  };
+  if (key === 'l') {
+    connection.write("Say: WOW!");
   };
 }
 
