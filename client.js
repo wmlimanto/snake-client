9a5b9af553756e2b15ffae6c887d9771d3b30735
Winston Limanto <wmlimanto@gmail.com>
2022-04-16 17:29:24 -0700

    added event on successful connection && send name to server

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
+  //create an object conn that represents connection with server
   const conn = net.createConnection({
     host: '192.168.1.47',
     port: 50541
@@ -14,6 +15,13 @@ const connect = function () {
   //added event for when ded
   conn.on("data", () => {
     console.log("you ded cuz you idled bruh")
+  });
+
+  //added event for when connection successful
+  conn.on("connect", () => {
+    console.log("connection successful-- let's SNAKE!");
+    //send our name to server after connecting
+    conn.write('Name: WML');
   });
 
   return conn;
