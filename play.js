c5ecd91d3c5a029e35cde585890b97a6c43cf8d3
Winston Limanto <wmlimanto@gmail.com>
2022-04-16 17:12:40 -0700

    refactored code-- imports connect function from client.js

--- play.js
+++ play.js
@@ -3,22 +3,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.1.47',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //added event for when ded
-  conn.on("data", () => {
-    console.log("you ded cuz you idled bruh")
-  });
-
-  return conn;
-};
+const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
